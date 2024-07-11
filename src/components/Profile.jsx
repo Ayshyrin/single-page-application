9c75359032e49f6eda61190856304e26769341c0
Ayshyrin Gurbanova <ayshyrinn@gmail.com>
2024-07-11 17:09:03 +0500

    Editing header

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8mcdA_uoJahxn3PQ-IC9WROV-GF2wuTl2FQ&s'/>
+            </div>
+            <div>
+                ava + descrip
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+      </div>
+    );
+};
+
+export default Profile;
